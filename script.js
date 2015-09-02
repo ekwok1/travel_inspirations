899a553457b7c70f92080ea4285b913b6573c46c
Eugene Kwok <eugenekwok11@gmail.com>
2015-09-02 14:45:05 -0700

    static first page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(function(){
-
+  buttonTest();
 });
+
+function buttonTest(){
+  $("#start").on("click", function(){
+    console.log("hi");
+  });
+}
 
 // insert fade in
 // <div id="title">Travel Inspirations</div>
